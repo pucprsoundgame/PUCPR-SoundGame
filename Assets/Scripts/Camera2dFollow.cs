3d72bca072023e76a1d6b5b5e251336088677637
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2018-08-06 03:24:04 -0300

    Included imgs, reorganized folders, Included camera 2d control, upgrade unity.

---
+++ Assets/Scripts/Camera2dFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Camera2dFollow : MonoBehaviour {
+
+	/// <summary>
+	/// Target to follow.
+	/// </summary>
+	[SerializeField] private Transform target;
+	
+	
+	void Update () {
+		transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+	}
+}
